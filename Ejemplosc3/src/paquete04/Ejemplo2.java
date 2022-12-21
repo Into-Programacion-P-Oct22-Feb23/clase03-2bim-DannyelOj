9ed0ec90758ce3f7fd9d807d5e7d0095b7f8e2f1
Daniel Ojeda <dmojeda@utpl.edu.ec>
2022-12-21 10:54:49 -0500

    subiendo

--- Ejemplosc3/src/paquete04/Ejemplo2.java
+++ Ejemplosc3/src/paquete04/Ejemplo2.java
@@ -16,12 +16,32 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
-        
-        double[][] dato1 = {{1, 2, 3},{6, 8, 9}}; //
+        int f;
+        int c;
+        int[][] dato1 = {{1, 2, 3},{6, 8, 9}}; 
+        int[][] dato2 = new int[2][3]; //
         /*
             1   4   3
             36  64  9
         */
+        for(f=0; f<dato1.length; f++){
+            for(c=0; c<dato1[f].length; c++){ 
+                if(dato1[f][c]%2==0){
+                    dato2[f][c] = dato1[f][c]*dato1[f][c];
+                }else{
+                   dato2[f][c] = dato1[f][c];
+                }   
+            }
+            
+        }
+        
+        for(f=0; f<dato1.length; f++){
+            for(c=0; c<dato1[f].length; c++){ 
+                System.out.printf("%d\t", dato2[f][c]);
+            }
+            System.out.println(); // salto de línea
+        }
+        System.out.println("Daniel Ojeda");
         
     }
     
